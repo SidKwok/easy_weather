5865eb17fa2e4c2d5b79dccbd8a87f0d7940b303
Sid Kwok <oceankwok@hotmail.com>
2016-03-07 22:11:25 +0800

    init weather animation

--- js/easyWeather.js
+++ js/easyWeather.js
@@ -5,7 +5,7 @@ var script= document.createElement('script');
 var url;
 script.type= 'text/javascript';  
 script.onload = function(){  
-    alert(remote_ip_info["city"]);
+    // alert(remote_ip_info["city"]);
     url = "http://wthrcdn.etouch.cn/weather_mini?city=" + remote_ip_info["city"]
  }  
 script.src= 'http://int.dpool.sina.com.cn/iplookup/iplookup.php?format=js&ip=';  
