761e8928765258510550dfb1e9eab1baded10c9a
sid <oceankwok@hotmail.com>
2016-03-28 10:13:38 +0800

    ud index

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,16 @@
 var gulp = require('gulp'),
-    uglify = require('gulp-uglify');
+    uglify = require('gulp-uglify'),
+    jshint = require('gulp-jshint');
 
 gulp.task('minifyjs', function(){
   return gulp.src('src/easyWeather.js')
       .pipe(uglify())
       .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('lint', function() {
+  return gulp.src('src/easyWeather.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'))
+    .pipe(notify({ message: 'lint task ok' }));
 });
